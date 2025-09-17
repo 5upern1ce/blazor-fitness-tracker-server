f678c53faa3113f5f8e9a890611f3f153018f114
Supern_Ice <63971176+5upern1ce@users.noreply.github.com>
2025-09-17 12:28:12 +0100

    moved things and stuff

--- obj/Debug/net9.0/blazor-fitness-tracker-server.AssemblyInfo.cs
+++ obj/Debug/net9.0/blazor-fitness-tracker-server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazor-fitness-tracker-server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+100b663ea4e524e66ce2ae5b2c9cba56590333a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazor-fitness-tracker-server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazor-fitness-tracker-server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
